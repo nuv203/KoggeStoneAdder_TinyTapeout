12d400a48fd16f1f6fab85ad2ad4ed9a805093b0
nuv203 <140340059+nuv203@users.noreply.github.com>
2024-11-01 15:39:41 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,9 +1,6 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 module tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave.
@@ -17,35 +14,32 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [3:0] a,b;
-  reg [7:0] uio_in;
-  wire [3:0] sum;
-  wire carry_out;
-  wire [2:0] uo_dum;
+  reg [7:0] b;           
+  reg [7:0] a;
+  wire [7:0] sum;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_koggestone_adder4 user_project (
-
-      // Include power ports for the Gate Level test:
+  // Instantiate the 8-bit Kogge-Stone adder module:
+  tt_um_koggestone_adder8 user_project (
 `ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
-
-      .ui_in  ({b,a}),    // Dedicated inputs
-      .uo_out ({uo_dum,carry_out,sum}),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+    .ui_in  (a),       // 8-bit input (concatenated a and b)
+    .uo_out (sum),      // 8-bit output for the sum
+    .uio_in (b),     // IO path input
+    .uio_out(uio_out),     // IO path output
+    .uio_oe (uio_oe),      // IO enable path
+    .ena    (ena),         // Enable signal
+    .clk    (clk),         // Clock signal
+    .rst_n  (rst_n)        // Reset (active low)
   );
 
-endmodule
+ 
+  endmodule
